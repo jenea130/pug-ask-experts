653c3f8be81c7e676c88ff68a5299177b5ebadee
jenea130 <jenea130@mail.ru>
2024-02-04 20:31:53 +0200

    feat: services

--- build/assets/jquery/custom-jquery.js
+++ build/assets/jquery/custom-jquery.js
@@ -1,3 +1,42 @@
-jQuery(document).ready(function ($) {
+$(document).ready(function () {
+  const slider = $(".services__wrapper");
 
+  if (slider) {
+    servicesSlider();
+  }
+  function servicesSlider() {
+    slider.slick({
+      infinite: true,
+      slidesToShow: 3,
+      slidesToScroll: 1,
+      speed: 1000,
+      arrows: false,
+      dots: false,
+      autoplay: true,
+      autoplaySpeed: 2000,
+      easing: "ease",
+      responsive: [
+        {
+          breakpoint: 992,
+          settings: {
+            slidesToShow: 2,
+          },
+        },
+        {
+          breakpoint: 576,
+          settings: {
+            slidesToShow: 1,
+          },
+        },
+      ],
+    });
+    const services__arrow_prev = $(".services__arrow--prev");
+    const services__arrow_next = $(".services__arrow--next");
+    services__arrow_prev.on("click", function () {
+      slider.slick("slickPrev");
+    });
+    services__arrow_next.on("click", function () {
+      slider.slick("slickNext");
+    });
+  }
 });
