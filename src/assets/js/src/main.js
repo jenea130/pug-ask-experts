c63fc1a4d1e9f31bc83bfd8343ae252fdd8f0256
jenea130 <jenea130@mail.ru>
2024-02-18 22:34:49 +0200

    feat: history and video

--- src/assets/js/src/main.js
+++ src/assets/js/src/main.js
@@ -1,11 +1,17 @@
 import toggleMenu from "./modules/header/toggleMenu";
 import togglePrice from "./modules/togglePrice";
+import videoPlay from "./modules/video/videoPlay";
 
 window.addEventListener("DOMContentLoaded", function () {
     const plan = document.querySelector(".plan") 
+    const video = document.querySelector(".video")
 
     if (plan) {
         togglePrice();
+    }
+
+    if (video) {
+        videoPlay();
     }
 
     toggleMenu();
