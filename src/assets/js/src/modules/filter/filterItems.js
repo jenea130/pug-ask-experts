770b39ebdbe9904a6f17dedd1bc991cb3d59627c
jenea130 <jenea130@mail.ru>
2024-02-29 16:04:38 +0200

    feat: page-intro blog

--- src/assets/js/src/modules/filter/filterItems.js
+++ src/assets/js/src/modules/filter/filterItems.js
@@ -1,16 +1,25 @@
-export default function() {
-   const filterImg = document.querySelectorAll('.taxes__img');
-   
-   document.querySelector(".taxes__header").addEventListener('click', event => {
-     if (event.target.tagName !== 'LI') return false; 
+export default function () {
+  const filter_img = document.querySelectorAll(".taxes__img");
+  const taxes_items = document.querySelectorAll(".taxes__item");
 
-     let filterClass = event.target.dataset['target'];
-     
-     filterImg.forEach( elem => {
-        elem.classList.remove('hide');
-        if (!elem.classList.contains(filterClass) && filterClass !== 'all') {
-            elem.classList.add('hide');
+  taxes_items.forEach((item) => {
+    item.addEventListener("click", (event) => {
+      let filterClass = item.dataset["target"];
+
+      resetActive();
+      item.classList.add("active");
+
+      filter_img.forEach((elem) => {
+        elem.classList.remove("hide");
+        if (!elem.classList.contains(filterClass) && filterClass !== "all") {
+          elem.classList.add("hide");
         }
-     });
-   });
+      });
+    });
+  });
+  function resetActive() {
+    taxes_items.forEach((item) => {
+      item.classList.remove("active");
+    });
+  }
 }
